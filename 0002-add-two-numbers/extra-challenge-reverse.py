de62f72b46930719a955b8e85d2c410177d2b295
ahmetakyurt <129226079+ahmetakyurt@users.noreply.github.com>
2026-02-10 15:16:04 +0300

    Implement addTwoNumbers with reverse version

---
+++ 0002-add-two-numbers/extra-challenge-reverse.py
@@ -0,0 +1,37 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverse(self, head):
+        prev = None
+        curr = head
+
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+
+        return prev
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0)
+        curr = dummy
+        excess = 0
+        while l1 or l2 or excess:
+            n1 = l1.val if l1 else 0
+            n2 = l2.val if l2 else 0
+            total = n1 + n2 + excess
+            excess = total //10
+            total = total %10
+
+            curr.next = ListNode(total)
+            curr = curr.next
+
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+
+        dummy = dummy.next
+        return self.reverse(dummy)
+
