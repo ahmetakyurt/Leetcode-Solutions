5d4349007933c4430b66e633e4e02aa34da39657
ahmetakyurt <129226079+ahmetakyurt@users.noreply.github.com>
2026-02-18 16:16:40 +0300

    Time: 0 ms (100%), Space: 19.6 MB (58.14%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,26 +1,36 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        m = len(nums1)
-        n = len(nums2)
+        # Her zaman daha kısa olan dizi üzerinde binary search yapalım (Verimlilik için)
+        A, B = nums1, nums2
+        total = len(A) + len(B)
+        half = total // 2
         
-        sortedList = list()
-        left = 0
-        right = n
-
-
-        p1, p2 = 0, 0
-        while p1 < m and p2 < n:
-            if nums1[p1] < nums2[p2]:
-                sortedList.append(nums1[p1])
-                p1 += 1
+        if len(B) < len(A):
+            A, B = B, A
+            
+        l, r = 0, len(A) - 1
+        while True:
+            # i: A dizisindeki kesim noktası (index)
+            # j: B dizisindeki kesim noktası
+            i = (l + r) // 2 
+            j = half - i - 2 # -2 çünkü indeksler 0'dan başlıyor
+            
+            # Kenar durumları (Sınırları aşarsak eksi/artı sonsuz varsayıyoruz)
+            Aleft = A[i] if i >= 0 else float("-infinity")
+            Aright = A[i + 1] if (i + 1) < len(A) else float("infinity")
+            Bleft = B[j] if j >= 0 else float("-infinity")
+            Bright = B[j + 1] if (j + 1) < len(B) else float("infinity")
+            
+            # Doğru kesim noktasını bulduk mu?
+            if Aleft <= Bright and Bleft <= Aright:
+                # Toplam eleman sayısı tek ise
+                if total % 2:
+                    return min(Aright, Bright)
+                # Toplam eleman sayısı çift ise
+                return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
+            
+            # Kesim noktasını kaydır
+            elif Aleft > Bright:
+                r = i - 1
             else:
-                sortedList.append(nums2[p2])
-                p2 += 1
-
-        sortedList.extend(nums1[p1:])
-        sortedList.extend(nums2[p2:])
-        sum = n+m
-        print(sortedList)
-        if (n+m) % 2 == 0:
-            return (sortedList[sum//2-1] + sortedList[sum//2])/2
-        return sortedList[sum//2]
+                l = i + 1
